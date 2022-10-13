e13ef0a2e8bf2f258ca2b39e48d52017fe31728b
Anthony Gordon <38927323+MrTonyGrey@users.noreply.github.com>
2022-10-13 15:53:41 -0400

    adding details screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,28 @@
 import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
