88f88fd00aeaa527b60a0b3d77fb1a57fae91369
Anthony Gordon <38927323+MrTonyGrey@users.noreply.github.com>
2022-10-17 19:03:06 -0400

    complete stack navigation

--- frontend/grover-app/App.js
+++ frontend/grover-app/App.js
@@ -1,14 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import * as React from 'react';
+
+import { StyleSheet, Text, View,ScrollView,SafeAreaView, Button } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const HomeScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Home Screen</Text>
+        <Button
+          title="Go to details screen"
+          onPress={() => navigation.navigate("Details")}
+        />
       <StatusBar style="auto" />
     </View>
   );
+};
+
+const DetailsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: 'green'
+        },
+        headerTintColor: 'yellow',
+        headerTitleStyle: {
+          fontWeight: 'bold'
+        }
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Details" component={DetailsScreen}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +61,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
